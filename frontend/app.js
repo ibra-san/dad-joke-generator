0416ada56308227299bac35bb4093a5ae542bedf
Ibrahim san <75019839+ibra-san@users.noreply.github.com>
2023-07-30 04:14:24 -0700

    Changed the API
    Used different api
    removed backend as its not needed

--- frontend/app.js
+++ frontend/app.js
@@ -1,38 +1,38 @@
 
-const fetchJokeButton = document.querySelector("#fetch_joke_button")
-const toggle_preloader = document.querySelector("#toggle_preloader")
+const fetchJokeButton = document.querySelector("#fetch_joke_button") // selecting the joke button
+const toggle_preloader = document.querySelector("#toggle_preloader") // The preloader gif
 
 
-const displayJoke = (isSuccess, joke) => { 
-    const jokeText = document.querySelector('#joke_text'); 
-    if(isSuccess && joke) { 
-        let setup = joke[0].setup; 
-        let punchline = joke[0].punchline;
-        jokeText.innerHTML = setup +" "+ punchline;
+const displayJoke = (status, joke) => {  // Function that displays the joke on the screen
+    const jokeText = document.querySelector('#joke_text'); // Selects the joke text area
+    if(status == 200 && joke) {  // Checks if the joke has been fetched or an error has returned
+        jokeText.innerHTML = joke;
+    } else { 
+        jokeText.innerHTML = status
     }
 }
 
-const fetchJoke = async () => { 
-    
-    fetch('https://dad-joke-generator-app.onrender.com', { 
-        method: "POST", 
+
+/* We are going to use another API to fetch jokes since the other one has more limits, and the jokes are super lame and bad. API is at: https://icanhazdadjoke.com/api  
+
+The steps are simple, you just need to create a AJAX function and fetch the joke from the API. But you need to have a header in order to choose the format of the fetched data. The fetched data by default is in HTML format
+
+
+*/
+
+const fetchJoke = async () => { // fetchs the joke
+
+    const response = await fetch("https://icanhazdadjoke.com" , 
+    {
         headers: { 
-            "Content-Type":"application/json", 
-            "Accept": "application/json"
-        }
-    }) .then(toggle_preloader.classList.add("preloader"))
-        .then(response => response.json())
-        .then(data => {
-            toggle_preloader.classList.remove("preloader")
-            console.log(data)
-            displayJoke(data.success, data.body) 
-        })
-        .catch((error) => { 
-            console.error("Error:", error)
-
-        })
-
+            Accept: "application/json", // Requesting json format from the api
+        },
+    })
+    const joke = await response.json()
+    console.log(joke)
+    displayJoke(joke.status, joke.joke) // calling the display function and passing the fetched jokes 
+    return joke
 }
 
-fetchJokeButton.addEventListener('click', fetchJoke)
-
+fetchJoke() // This for when the page just starts up. 
+fetchJokeButton.addEventListener('click', fetchJoke) // Fetchs a joke on button click
